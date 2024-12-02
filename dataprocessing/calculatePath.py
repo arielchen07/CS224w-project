71c6c961110d3542c58634000ace91157c8fcad1
Tonyzhang2000 <zhangshutong2000@gmail.com>
2024-12-02 01:34:31 -0800

    add data generation

--- dataprocessing/calculatePath.py
+++ dataprocessing/calculatePath.py
@@ -1,10 +1,11 @@
 import json
+import os
 from typing import List, Optional
 import searchUtil
+import argparse
+from tqdm import tqdm
 from mapUtil import (
     CityMap,
-    createStanfordMap,
-    getTotalCost,
     createMap
 )
 from search import (
@@ -17,37 +18,72 @@ def extractPath(startLocation: str, search: searchUtil.SearchAlgorithm) -> List[
     return [startLocation] + search.actions
 
 
-def printPath(
+def saveResultPath(
     path: List[str],
     waypointTags: List[str],
     cityMap: CityMap,
-    outPath: Optional[str] = "path.json",
-):
-    doneWaypointTags = set()
+    outPath: Optional[str],
+    startLocation,
+    endTag
+):  
+    doneWaypointTags = []
+    # print(waypointTags)
+    # print(path)
     for location in path:
         for tag in cityMap.tags[location]:
             if tag in waypointTags:
-                doneWaypointTags.add(tag)
-        # tagsStr = " ".join(cityMap.tags[location])
-        # doneTagsStr = " ".join(sorted(doneWaypointTags))
-        # print(f"Location {location} tags:[{tagsStr}]; done:[{doneTagsStr}]")
-    # print(f"Total distance: {getTotalCost(path, cityMap)}")
+                doneWaypointTags.append(tag.split('=')[1])
+
     if outPath is not None:
         with open(outPath, "w") as f:
-            data = {"waypointTags": waypointTags, "path": path}
+            data = {"start": startLocation,
+                    "waypointTags": waypointTags, 
+                    "end": endTag.split('=')[1]
+                    }
             json.dump(data, f, indent=2)
 
 
-def calculatePath():
+def calculatePath(minNum, maxNum, saveId, savePath):
     """Given custom WaypointsShortestPathProblem, find the minimun path and prepare visualization."""
-    cityMap = createMap("stanford.pbf")
-    startLocation, waypointTags, endTag = generateRandomPath(cityMap, minWayPoints=2, maxWayPoints=3)
+    cityMap = createMap("../data/stanford.pbf")
+    startLocation, waypointTags, endTag = generateRandomPath(cityMap, minWayPoints=minNum, maxWayPoints=maxNum)
     problem = WaypointsShortestPathProblem(startLocation, tuple(sorted(waypointTags)), str(endTag), cityMap)
 
     ucs = searchUtil.UniformCostSearch(verbose=0)
     ucs.solve(problem)
     path = extractPath(problem.startLocation, ucs)
-    printPath(path=path, waypointTags=problem.waypointTags, cityMap=cityMap)
+    saveResultPath(path=path, waypointTags=problem.waypointTags, cityMap=cityMap, outPath=f"{savePath}/{saveId}.json", startLocation=startLocation, endTag=endTag)
 
 if __name__ == "__main__":
-    calculatePath()
+    parser = argparse.ArgumentParser(description="Add data generation hyper parameters")
+
+    # Add arguments
+    parser.add_argument(
+        "--minWayPoints", 
+        type=int, 
+        default=6, 
+        help="Minimum number of way points"
+    )
+    parser.add_argument(
+        "--maxWayPoints", 
+        type=int, 
+        default=8, 
+        help="Maximum number of way points"
+    )
+    parser.add_argument(
+        "--numPath", 
+        type=int, 
+        default=1000, 
+        help="Number of data points to generate"
+    )
+    parser.add_argument(
+        "--savePath", 
+        type=str, 
+        default="out", 
+        help="Number of data points to generate"
+    )
+
+    args = parser.parse_args()
+    os.makedirs(args.savePath, exist_ok=True)
+    for num in tqdm(range(args.numPath)):
+        calculatePath(args.minWayPoints, args.maxWayPoints, num, args.savePath)
