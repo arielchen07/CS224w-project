2357c13326be38c1728a6efbb0036a0d142d9193
arielchen07 <sirui.chen@mail.utoronto.ca>
2024-12-09 21:11:51 -0800

    try loss

--- train_tp_small.py
+++ train_tp_small.py
@@ -299,7 +299,7 @@ for epoch in range(num_epochs):
         print(f"waypoints_correct: {waypoints_correct.shape}")
         # Compute loss
         # loss = loss_fn(predicted_ordering, waypoints_correct.float())
-        loss = loss_fn(predicted_ordering, waypoints_correct)
+        loss = loss_fn(predicted_ordering, waypoints_correct.float())
 
 
         # Backpropagation
